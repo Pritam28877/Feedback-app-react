1c29ee78363ed6ba6eae1454ed6bd1e2a9e1c220
Pritam Das <dpritam28577@gmail.com>
2023-08-18 16:08:51 +0530

    add the Feedback component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-// import { useState } from "react";
-
 import "./App.css";
+import FeedbackItem from "./companent/FeedbackItem";
 import Header from "./companent/Header";
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
-      <Header  />
+      <Header />
+      <FeedbackItem />
     </>
   );
 }
