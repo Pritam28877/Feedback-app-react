8a379c743ec5f74c9d90c5bf4293cae9c13752a5
Pritam Das <dpritam28577@gmail.com>
2023-08-19 12:14:32 +0530

    add the btn components

--- src/components/shared/Buton.jsx
+++ src/components/shared/Buton.jsx
@@ -1,12 +1,15 @@
-import PropTypes form "
-const Buton = ({ children, version, type, isDisable }) => {
+import PropTypes from "prop-types";
+const Buton = ({ children, version, type, isDisabled }) => {
   return (
-    <button type={type} disabled={isDisable} className={`btn btn-${version}`}>
+    <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
       {children}
     </button>
   );
 };
 Buton.prototype = {
-    children : PropTypes.node.isRe
-}
+  children: PropTypes.node.isRequired,
+  version: PropTypes.string,
+  type: PropTypes.string,
+  isDisabled: PropTypes.bool,
+};
 export default Buton;
